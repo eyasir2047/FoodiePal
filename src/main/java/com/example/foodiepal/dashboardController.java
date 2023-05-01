3b13e302491939bbd5dda5f70c3f8c765d7f2d4d
eyasir2047 <eyasir2047@gmail.com>
2023-05-01 22:26:34 +0600

    work of available foods/drinks is completed!!

--- src/main/java/com/example/foodiepal/dashboardController.java
+++ src/main/java/com/example/foodiepal/dashboardController.java
@@ -2,6 +2,8 @@ package com.example.foodiepal;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
@@ -25,6 +27,8 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.util.function.Predicate;
+
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
@@ -237,7 +241,10 @@ public class dashboardController implements Initializable {
                     alert.setContentText("Successfully Added|");
                     alert.showAndWait();
 
+                    //to show the data
                     availableFDShowData();
+                    //to clear the fields
+                    availableFDClear();
 
                 }
 
@@ -251,11 +258,166 @@ public class dashboardController implements Initializable {
         }
     }
 
+    public void availableFDUpdate(){
+        String sql="UPDATE category SET product_name ='"
+                +availableFD_productName.getText()+ "',type='"
+                + availableFD_productType.getSelectionModel().getSelectedItem()+"',price='"
+                +availableFD_productPrice.getText()+"',status='"
+                +availableFD_productStatus.getSelectionModel().getSelectedItem()
+                +"' WHERE product_id='"+availableFD_productID.getText()+"'";
+
+        connect=database.connectDb();
+
+        try{
+
+
+            Alert alert;
+
+            if(availableFD_productID.getText().isEmpty()
+            || availableFD_productName.getText().isEmpty()
+            || availableFD_productType.getSelectionModel().getSelectedItem()==null
+            || availableFD_productPrice.getText().isEmpty()
+            || availableFD_productStatus.getSelectionModel().getSelectedItem()==null){
+
+                alert=new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error Message");
+                alert.setHeaderText(null);
+                alert.setContentText("Please fill all the blank fields");
+                alert.showAndWait();
+
+            }
+            else{
+
+
+                alert=new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle("Confirmation Message");
+                alert.setHeaderText(null);
+                alert.setContentText("Are you sure you want to UPDATE Product ID: "
+                +availableFD_productID.getText()+"?");
+
+                Optional<ButtonType> option=alert.showAndWait();
+
+                if(option.get().equals(ButtonType.OK)){
+
+                    alert=new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Information Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Successfully Updated!");
+                    alert.showAndWait();
+
+
+                    statement=connect.createStatement();
+                    statement.executeUpdate(sql);
+
+                    //to show the data
+                    availableFDShowData();
+                    //to clear the fields
+                    availableFDClear();
+
+                }
+                else{
+                    alert=new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Information Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Cancelled.");
+                    alert.showAndWait();
+
+
+                }
+
+
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void availableFDDelete() {
+        String sql = "DELETE FROM category WHERE product_id ='"
+                + availableFD_productID.getText() + "'";
+
+        connect = database.connectDb();
+        try {
+
+
+            Alert alert;
+
+            if (availableFD_productID.getText().isEmpty()
+                    || availableFD_productName.getText().isEmpty()
+                    || availableFD_productType.getSelectionModel().getSelectedItem() == null
+                    || availableFD_productPrice.getText().isEmpty()
+                    || availableFD_productStatus.getSelectionModel().getSelectedItem() == null) {
+
+                alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error Message");
+                alert.setHeaderText(null);
+                alert.setContentText("Please fill all the blank fields");
+                alert.showAndWait();
+
+            } else {
+
+
+                alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle("Confirmation Message");
+                alert.setHeaderText(null);
+                alert.setContentText("Are you sure you want to DELETE Product ID: "
+                        + availableFD_productID.getText() + "?");
+
+                Optional<ButtonType> option = alert.showAndWait();
+
+                if (option.get().equals(ButtonType.OK)) {
+
+                    alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Information Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Successfully Deleted!");
+                    alert.showAndWait();
+
+
+                    statement = connect.createStatement();
+                    statement.executeUpdate(sql);
+
+                    //to show the data
+                    availableFDShowData();
+                    //to clear the fields
+                    availableFDClear();
+
+                } else {
+                    alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Information Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Cancelled.");
+                    alert.showAndWait();
+
+
+                }
+
+
+            }
+        }
+        catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
+    public void availableFDClear(){
+        availableFD_productID.setText("");
+        availableFD_productName.setText("");
+        availableFD_productType.getSelectionModel().clearSelection();
+        availableFD_productPrice.setText("");
+        availableFD_productStatus.getSelectionModel().clearSelection();
+
+    }
+
+
+
     public ObservableList<categories> availableFDListData(){
 
-        ObservableList<categories>listData=FXCollections.observableArrayList();
+       //ObservableList<categories>listData=FXCollections.observableArrayList();
+        final ObservableList<categories>listData=FXCollections.observableArrayList();
 
-        String sql="SELECT* FROM category";
+        String sql="SELECT * FROM category";
 
         connect=database.connectDb();
 
@@ -282,21 +444,131 @@ public class dashboardController implements Initializable {
 
     }
 
-    private  ObservableList<categories>availableFDList;
-    public  void availableFDShowData(){
-        availableFDList=availableFDListData();
+//    public void availableFDSearch(){
+//
+//
+//
+//       FilteredList<categories>filter=new FilteredList<>(availableFDList,e->true);
+//
+//
+//
+//
+//          availableFD_search.textProperty().addListener((observabl, newValue, oldValue) -> {
+//
+//                       filter.setPredicate(predicateCategories -> {
+//
+//
+//                           if ( newValue == null || newValue.isEmpty() ) {
+//                                return true;
+//                            }
+//
+//
+//
+//                           String searchKey = newValue.toLowerCase();
+//
+//                            if (predicateCategories.getProductId().toLowerCase().contains(searchKey)) {
+//                                return true;
+//                            } else if (predicateCategories.getName().toLowerCase().contains(searchKey)) {
+//                                return true;
+//                            } else if (predicateCategories.getType().toLowerCase().contains(searchKey)) {
+//                                return true;
+//                            } else if (predicateCategories.getPrice().toString().contains(searchKey)) {
+//                                return true;
+//                            } else if (predicateCategories.getStatus().toLowerCase().contains(searchKey)) {
+//                                return true;
+//                            } else {
+//                                return false;
+//                            }
+////
+//
+//
+//
+//                            });
+//                });
+//
+//        SortedList<categories>sortList=new SortedList<>(filter);
+//        sortList.comparatorProperty().bind(availableFD_tableView.comparatorProperty());
+//
+//        availableFD_tableView.setItems(sortList);
+//
+//
+//
+//    }
+//
+//
+//
+//   private  ObservableList<categories>availableFDList;
+//
+//    public  void availableFDShowData(){
+//        availableFDList=availableFDListData();
+//        availableFD_col_productID.setCellValueFactory(new PropertyValueFactory<>("productId"));
+//        availableFD_col_productName.setCellValueFactory(new PropertyValueFactory<>("name"));
+//        availableFD_col_type.setCellValueFactory(new PropertyValueFactory<>("type"));
+//        availableFD_col_price.setCellValueFactory(new PropertyValueFactory<>("price"));
+//        availableFD_col_status.setCellValueFactory(new PropertyValueFactory<>("status"));
+//
+//
+//        availableFD_tableView.setItems(availableFDList);
+//
+//
+//
+//    }
+
+
+    ///-----------chatgpt
+
+    private FilteredList<categories> filteredAvailableFDList;
+    private SortedList<categories> sortedAvailableFDList;
+
+    public void availableFDShowData() {
+
+        ObservableList<categories> availableFDList = availableFDListData();
         availableFD_col_productID.setCellValueFactory(new PropertyValueFactory<>("productId"));
         availableFD_col_productName.setCellValueFactory(new PropertyValueFactory<>("name"));
         availableFD_col_type.setCellValueFactory(new PropertyValueFactory<>("type"));
         availableFD_col_price.setCellValueFactory(new PropertyValueFactory<>("price"));
         availableFD_col_status.setCellValueFactory(new PropertyValueFactory<>("status"));
 
+        filteredAvailableFDList = new FilteredList<>(availableFDList, (category) -> true);
+        sortedAvailableFDList = new SortedList<>(filteredAvailableFDList);
+        sortedAvailableFDList.comparatorProperty().bind(availableFD_tableView.comparatorProperty());
 
-        availableFD_tableView.setItems(availableFDList);
+        availableFD_tableView.setItems(sortedAvailableFDList);
+    }
+
+    public void availableFDSearch() {
+        String searchKey = availableFD_search.getText().toLowerCase();
+
+        filteredAvailableFDList.setPredicate((category) -> {
+            if (searchKey.isEmpty()) {
+                return true;
+            }
+            return category.getProductId().toLowerCase().contains(searchKey)
+                    || category.getName().toLowerCase().contains(searchKey)
+                    || category.getType().toLowerCase().contains(searchKey)
+                    || category.getPrice().toString().contains(searchKey)
+                    || category.getStatus().toLowerCase().contains(searchKey);
+        });
+    }
+
+
+    //------------end
+
+    public void availableFDSelect(){
+        categories catData=availableFD_tableView.getSelectionModel().getSelectedItem();
+        int num=availableFD_tableView.getSelectionModel().getSelectedIndex();
+
+        if((num-1)<-1){
+            return;
+        }
+        availableFD_productID.setText(catData.getProductId());
+        availableFD_productName.setText(catData.getName());
+        availableFD_productPrice.setText(String.valueOf(catData.getPrice()));
 
 
 
     }
+
 
 
 
@@ -350,6 +622,7 @@ public class dashboardController implements Initializable {
             order_btn.setStyle("-fx-background-color: transparent;-fx-border-width:1px;-fx-text-fill:#000;");
 
             availableFDShowData();
+            availableFDSearch();
 
         }
         else if(event.getSource()==order_btn){
@@ -435,5 +708,6 @@ public class dashboardController implements Initializable {
         availableFDStatus();
         availableFDType();
         availableFDShowData();
+
     }
 }
